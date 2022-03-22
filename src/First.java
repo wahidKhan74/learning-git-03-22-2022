eb1ce5c25b654159c9411f4bcaaf15984626c996
waheed khan <wahid---@gmail.com>
2022-03-22 10:05:44 +0530

    added sample and first java file

---
+++ src/First.java
@@ -0,0 +1,9 @@
+class First {
+    /**
+        Sample Program
+     */
+    public static void main(String args) {
+
+        System.out.println("Hello Java Prgram");
+    }
+}
---
+++ src/Sample.java
@@ -0,0 +1,10 @@
+class Sample {
+
+     /**
+        Sample Program
+     */
+    public static void main(String args) {
+
+        System.out.println("Hello Sample Prgram");
+    }
+}
